896f32daa6ea11511743f75c541a74752a49170e
Raszero <travisjcelli95@gmail.com>
2024-02-02 12:21:41 -0600

    create User model

---
+++ models/Users.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    // regex format from 17-CS 02-challenge
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate: {
+        validator: function (v) {
+          return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+        },
+      },
+    },
+    // // link thoughts to user
+    // thoughts: [
+    //   {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "Thought",
+    //   },
+    // ],
+    // // link friends to User
+    // friends: [
+    //   {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "User",
+    //   },
+    // ],
+  },
+  //  convert to JSON
+  { toJSON: { virtuals: true } }
+);
+
+// initialize the User model
+const User = model('User', userSchema)
+// export model
+module.exports = User
