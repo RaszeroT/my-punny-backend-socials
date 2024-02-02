3a26a86e9e06685f4a0d918461733b83943c6743
Raszero <travisjcelli95@gmail.com>
2024-02-02 16:44:41 -0600

    set reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create reaction model
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minLength: 1
+        },
+        username: {
+            type: String,
+            required: true
+        },      
+    },
+    {timestamps: true,
+    toJSON: {
+        getters: true
+    }}
+)
+
+// initialize the post model
+const Reaction = model('Reaction', reactionSchema)
+//export model
+module.exports = Reaction;
